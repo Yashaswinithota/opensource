410410545ac7477128f805559adf3076bf6958b8
Yashaswinithota <yashaswinithota1@gmail.com>
2024-11-18 12:56:21 +0530

    Create dnd.py

---
+++ visaa_prep/dnd.py
@@ -0,0 +1,10 @@
+A, B = map(int,input().split())
+Arr = map(int,input().split())
+even_sum = 0
+odd_sum = 0
+for i in Arr:
+    if i % B == 0:
+        even_sum += i
+    else:
+        odd_sum += i
+print(even_sum - odd_sum)
